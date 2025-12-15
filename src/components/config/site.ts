e6c0da6e1657d9a11da5319b90a488a702c5f975
Zain <z3eiyn@gmail.com>
2025-12-15 16:04:25 +0700

    chore: site config & TS update

---
+++ src/components/config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "Zain",
+    url: "https://iamzeiyn.com",
+    description: "Portfolio website of Zain",
+    social: {
+        instagram: "https://instagram.com/iamzeiyn",
+        github: "https://github.com/zeiynz",
+        linkedin: "https://linkedin.com/in/zeiyn",
+        youtube: "https://www.youtube.com/@iamzeiyn",
+    },
+}
